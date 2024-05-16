21dc97bc706ac3aa7b580008e7fb28c875474ef2
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-05-16 14:28:18 -0300

    lxde-core.sh

---
+++ lxde-core.sh
@@ -0,0 +1,5 @@
+apt install -y build-essential lsb-release module-assistant ca-certificates software-properties-common aptitude sudo wget git gnupg curl cmake g++ gcc make automake autoconf net-tools plocate lzip tar unzip
+
+apt install -y lxde-core
+
+apt install -y xrdp
