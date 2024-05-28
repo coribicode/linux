cbebe1617c31cce356646a0a8cb3dff3de910b42
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-05-28 01:55:29 -0300

    lxde-core.sh

--- lxde-core.sh
+++ lxde-core.sh
@@ -1,4 +1,4 @@
-apt install -y build-essential lsb-release module-assistant ca-certificates software-properties-common aptitude sudo wget git gnupg curl cmake g++ gcc make automake autoconf net-tools plocate lzip tar unzip
+apt install -y build-essential lsb-release apt-transport-https module-assistant ca-certificates software-properties-common aptitude sudo wget git gnupg curl cmake g++ gcc make automake autoconf net-tools plocate lzip tar unzip
 
 apt install -y lxde-core
 
