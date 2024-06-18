38265985995e591a6b992913588e24d8c673b4a3
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-06-18 17:42:49 -0300

    Delete lxde-core.sh

--- lxde-core.sh
+++
@@ -1,9 +0,0 @@
-apt install -y lxde-core
-
-apt install -y xrdp
-
-sudo update-initramfs -u
-## FIX BLACKSCREEN VM HYPER-V ##
-cp /etc/default/grub /etc/default/grub.bkp
-sed -i 's|GRUB_CMDLINE_LINUX_DEFAULT="quiet|GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nomodeset|g' /etc/default/grub
-sudo update-grub
