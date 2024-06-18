cfe1cd0ef7346dbc2918715651a50d7e685147f5
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-06-18 17:40:17 -0300

    essential.sh

---
+++ essential.sh
@@ -0,0 +1,1 @@
+apt install -y build-essential lsb-release apt-transport-https module-assistant ca-certificates software-properties-common aptitude sudo wget git gnupg curl cmake g++ gcc make automake autoconf net-tools plocate lzip tar unzip
