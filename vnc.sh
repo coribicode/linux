5d5bdb5606945189e320c4e9cefd28c1fd7399f9
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-06-20 01:21:54 -0300

    vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -11,6 +11,12 @@ pwd=123
 ####################################
 
 apt-get install x11vnc -y
+
+mkdir $PWD/.vnc
+echo $pwd | vncpasswd -f > $PWD/.vnc/passwd
+chown -R $USER:$USER $PWD/.vnc
+chmod 0600 $PWD/.vnc/passwd
+
 cat << 'EOF' >> /lib/systemd/system/x11vnc.service
 [Unit]
 Description=x11vnc service
@@ -18,7 +24,7 @@ After=display-manager.service
 network.target syslog.target
 [Service]
 Type=simple
-ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd $pwd 
+ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess
 ExecStop=/usr/bin/killall x11vnc
 Restart=on-failure
 [Install]
