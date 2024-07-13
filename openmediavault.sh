9ce67744e06b672a9e0d449c1e4c7353fb2e5572
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-13 19:06:57 -0300

    openmediavault.sh

--- openmediavault.sh
+++ openmediavault.sh
@@ -1,19 +1,5 @@
-apt-get install --yes gnupg
+wget https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install
 
-wget --quiet --output-document=- https://packages.openmediavault.org/public/archive.key | gpg --dearmor --yes --output "/usr/share/keyrings/openmediavault-archive-keyring.gpg"
+chmod +x install
 
-echo "deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] https://packages.openmediavault.org/public shaitan main" >> /etc/apt/sources.list.d/openmediavault.list
-
-apt update && apt upgrade
-
-export LANG=C.UTF-8
-export DEBIAN_FRONTEND=noninteractive
-export APT_LISTCHANGES_FRONTEND=none
-apt-get update
-apt-get --yes --auto-remove --show-upgraded \
-    --allow-downgrades --allow-change-held-packages \
-    --no-install-recommends \
-    --option DPkg::Options::="--force-confdef" \
-    --option DPkg::Options::="--force-confold" \
-    install openmediavault
-
+sudo ./install -b
