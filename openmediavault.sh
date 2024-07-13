8f6983e441487eb3acd9707deff628e489200bc9
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-13 18:56:13 -0300

    openmediavault.sh

--- openmediavault.sh
+++ openmediavault.sh
@@ -1,5 +1,7 @@
 echo "deb http://packages.openmediavault.org/public kralizec" main >> /etc/apt/sources.list.d/openmediavault.list
 
+apt update && apt upgrade
+
 apt install openmediavault-keyring postfix -y
 
 apt install openmediavault -y
