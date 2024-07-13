83ee1436ed5e3d1d49d08e3e9a3f5530988d9281
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-13 19:01:25 -0300

    openmediavault.sh

--- openmediavault.sh
+++ openmediavault.sh
@@ -1,8 +1,19 @@
-echo "deb http://packages.openmediavault.org/public kralizec" main >> /etc/apt/sources.list.d/openmediavault.list
+apt-get install --yes gnupg
+
+wget --quiet --output-document=- https://packages.openmediavault.org/public/archive.key | gpg --dearmor --yes --output "/usr/share/keyrings/openmediavault-archive-keyring.gpg"
+
+echo "deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] https://packages.openmediavault.org/public shaitan main" >> /etc/apt/sources.list.d/openmediavault.list
 
 apt update && apt upgrade
 
-apt install openmediavault-keyring postfix -y
-
-apt install openmediavault -y
+export LANG=C.UTF-8
+export DEBIAN_FRONTEND=noninteractive
+export APT_LISTCHANGES_FRONTEND=none
+apt-get update
+apt-get --yes --auto-remove --show-upgraded \
+    --allow-downgrades --allow-change-held-packages \
+    --no-install-recommends \
+    --option DPkg::Options::="--force-confdef" \
+    --option DPkg::Options::="--force-confold" \
+    install openmediavault
 
