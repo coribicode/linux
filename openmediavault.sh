7e1ba48c91440ca5ffb71dc35ef1ff027f886e52
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-14 18:20:13 -0300

    openmediavault.sh

--- openmediavault.sh
+++ openmediavault.sh
@@ -1,5 +1,30 @@
-wget https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install
+apt_dist='openmediavault'
 
-chmod +x install
+keyring_gpg_path="/usr/share/keyrings/"$apt_dist"-archive-keyring.gpg"
 
-sudo ./install -b
+key_uri='https://packages.openmediavault.org/public/archive.key'
+
+cat <<"EOL">> /opt/$apt_dist.list
+deb [signed-by=keyring_gpg_path] http://packages.openmediavault.org/public/ sandworm main
+deb [signed-by=keyring_gpg_path] https://openmediavault.github.io/packages/ sandworm main
+EOL
+
+cat "/opt/$apt_dist.list" >> /etc/apt/sources.list.d/$apt_dist.list
+
+sed -i "s|keyring_gpg_path|$keyring_gpg_path|g" /etc/apt/sources.list.d/$apt_dist.list
+
+apt-get install -y gnupg wget
+
+wget --quiet --output-document=- $key_uri | gpg --dearmor --yes --output "$keyring_gpg_path"
+
+export LANG=C.UTF-8
+export DEBIAN_FRONTEND=noninteractive
+export APT_LISTCHANGES_FRONTEND=none
+apt-get update
+apt-get --yes --auto-remove --show-upgraded \
+    --allow-downgrades --allow-change-held-packages \
+    --no-install-recommends \
+    --option DPkg::Options::="--force-confdef" \
+    --option DPkg::Options::="--force-confold" \
+    install openmediavault
+
