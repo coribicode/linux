c35e2290ab5583a268c3d97b17ffa59ec6c8082a
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-14 20:11:16 -0300

    openmediavault.sh

--- openmediavault.sh
+++ openmediavault.sh
@@ -4,6 +4,8 @@ keyring_gpg_path="/usr/share/keyrings/"$apt_dist"-archive-keyring.gpg"
 
 key_uri='https://packages.openmediavault.org/public/archive.key'
 
+ip=$(hostname -I | cut -d ' ' -f 1)
+
 cat <<"EOL">> /opt/$apt_dist.list
 deb [signed-by=keyring_gpg_path] http://packages.openmediavault.org/public/ sandworm main
 deb [signed-by=keyring_gpg_path] https://openmediavault.github.io/packages/ sandworm main
@@ -13,7 +15,7 @@ cat "/opt/$apt_dist.list" >> /etc/apt/sources.list.d/$apt_dist.list
 
 sed -i "s|keyring_gpg_path|$keyring_gpg_path|g" /etc/apt/sources.list.d/$apt_dist.list
 
-apt-get install -y gnupg wget systemd-timesyncd
+apt-get install -y wget gnupg sudo systemd-timesyncd 
 
 wget --quiet --output-document=- $key_uri | gpg --dearmor --yes --output "$keyring_gpg_path"
 
@@ -28,7 +30,29 @@ apt-get --yes --auto-remove --show-upgraded \
     --option DPkg::Options::="--force-confold" \
     install openmediavault
 
-ip=$(hostname -I | cut -d ' ' -f 1)
+sudo omv-salt deploy run systemd-networkd
+
+echo
+echo "Buscando as últimas atualizações do OpenMediaVault..."
+echo
+apt --only-upgrade install openmediavault
+sudo omv-upgrade
+echo
+
+echo
+echo "Instalando pacotes essencias do OpenMediaVault..."
+echo
+apt install -y \
+openmediavault-apt \
+openmediavault-clamav \
+openmediavault-diskstats \
+openmediavault-filebrowser \
+openmediavault-ftp \
+openmediavault-photoprism \
+openmediavault-snmp \
+openmediavault-usbbackup \
+openmediavault-wetty
+echo
 
 echo
 echo "Acesse http://$ip"
