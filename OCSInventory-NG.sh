be9f817fce0b4da0f371f95a158f7d954a1e8131
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-15 05:45:26 -0300

    OCSInventory-NG.sh

--- OCSInventory-NG.sh
+++ OCSInventory-NG.sh
@@ -61,9 +61,9 @@ cat $PHP_FILE | grep "max_input_time ="
 cat $PHP_FILE | grep "post_max_size ="
 
 cat >> /opt/ocs_config.sql << EOL
-CREATE DATABASE $OCS_DB;
-CREATE USER '$OCS_USER'@'localhost' IDENTIFIED BY '$OCS_PASSWORD';
-GRANT ALL PRIVILEGES on $OCS_DB.* TO '$OCS_USER'@'localhost';
+CREATE DATABASE $OCS_DB_NAME;
+CREATE USER '$OCS_DB_USER'@'localhost' IDENTIFIED BY '$OCS_DB_PASSWORD';
+GRANT ALL PRIVILEGES on $OCS_DB_NAME.* TO '$OCS_DB_USER'@'localhost';
 EOL
 
 sudo sed -i 's/"//g' /opt/ocs_config.sql
@@ -75,18 +75,22 @@ tar xvf /opt/OCSNG_UNIX_SERVER-$OCS_VERSION.tar.gz -C /var/www/
 
 cp $OCS_PATH/./setup.sh $OCS_PATH/./setup.sh.bkp
 
-sudo sed -i 's|DB_SERVER_PORT="${DB_SERVER_PORT:-3306}"|DB_SERVER_PORT=OCS_PORT|g' $OCS_PATH/./setup.sh
-sudo sed -i 's|DB_SERVER_USER="${DB_SERVER_USER:-ocs}"|DB_SERVER_USER=OCS_USER|g' $OCS_PATH/./setup.sh
-sudo sed -i 's|DB_SERVER_PWD="${DB_SERVER_PWD:-ocs}"|DB_SERVER_PWD=OCS_PASSWORD|g' $OCS_PATH/./setup.sh
+sudo sed -i 's|DB_SERVER_HOST="${DB_SERVER_HOST:-localhost}"|DB_SERVER_HOST="localhost"|g' $OCS_PATH/./setup.sh
 
-sudo sed -i "s|DB_SERVER_PORT=OCS_PORT|DB_SERVER_PORT="$OCS_DB_PORT"|g" $OCS_PATH/./setup.sh
-sudo sed -i "s|DB_SERVER_USER=OCS_USER|DB_SERVER_USER="$OCS_DB_USER"|g" $OCS_PATH/./setup.sh
-sudo sed -i "s|DB_SERVER_PWD=OCS_PASSWORD|DB_SERVER_PWD="$OCS_DB_PASSWORD"|g" $OCS_PATH/./setup.sh
+sudo sed -i 's|DB_SERVER_PORT="${DB_SERVER_PORT:-3306}"|DB_SERVER_PORT=OCS_DB_PORT|g' $OCS_PATH/./setup.sh
+sudo sed -i 's|DB_SERVER_USER="${DB_SERVER_USER:-ocs}"|DB_SERVER_USER=OCS_DB_USER|g' $OCS_PATH/./setup.sh
+sudo sed -i 's|DB_SERVER_PWD="${DB_SERVER_PWD:-ocs}"|DB_SERVER_PWD=OCS_DB_PASSWORD|g' $OCS_PATH/./setup.sh
 
-sudo sed -i 's|APACHE_BIN="${APACHE_BIN:-}"|APACHE_BIN="/usr/sbin/apache2ctl"|g' $OCS_PATH/./setup.sh
+sudo sed -i "s|DB_SERVER_PORT=OCS_DB_PORT|DB_SERVER_PORT="$OCS_DB_PORT"|g" $OCS_PATH/./setup.sh
+sudo sed -i "s|DB_SERVER_USER=OCS_DB_USER|DB_SERVER_USER="$OCS_DB_USER"|g" $OCS_PATH/./setup.sh
+sudo sed -i "s|DB_SERVER_PWD=OCS_DB_PASSWORD|DB_SERVER_PWD="$OCS_DB_PASSWORD"|g" $OCS_PATH/./setup.sh
 
-export APACHE_BIN=/usr/sbin/apache2ctl
-export APACHE_BIN_FOUND=/usr/sbin/apache2ctl
+APACHE_BIN='"/usr/sbin/apache2ctl"'
+
+sudo sed -i 's|APACHE_BIN="${APACHE_BIN:-}"|APACHE_BIN=APACHE_BIN|g' $OCS_PATH/./setup.sh
+sudo sed -i "s|APACHE_BIN=APACHE_BIN|APACHE_BIN="$APACHE_BIN"|g" $OCS_PATH/./setup.sh
+
+# export APACHE_BIN=/usr/sbin/apache2ctl
 
 cd $OCS_PATH
 
@@ -95,29 +99,29 @@ echo "y" | sh ./setup.sh
 cp /etc/apache2/conf-available/z-ocsinventory-server.conf /etc/apache2/conf-available/z-ocsinventory-server.conf.bkp
 cp /etc/apache2/conf-available/zz-ocsinventory-restapi.conf /etc/apache2/conf-available/zz-ocsinventory-restapi.conf.bkp
 
-OCS_PORT=$(echo $OCS_PORT | cut -d '"' -f2)
-OCS_DB=$(echo $OCS_DB | cut -d '"' -f2)
-OCS_USER=$(echo $OCS_USER | cut -d '"' -f2)
-OCS_PASSWORD=$(echo $OCS_PASSWORD | cut -d '"' -f2)
+OCS_DB_NAME=$(echo $OCS_DB_NAME | cut -d '"' -f2)
+OCS_DB_PORT=$(echo $OCS_DB_PORT | cut -d '"' -f2)
+OCS_DB_USER=$(echo $OCS_DB_USER | cut -d '"' -f2)
+OCS_DB_PASSWORD=$(echo $OCS_DB_PASSWORD | cut -d '"' -f2)
 
-export PTH_ZZ_OCS_RESTAPI=/etc/apache2/conf-available/zz-ocsinventory-restapi.conf
+PTH_ZZ_OCS_RESTAPI=/etc/apache2/conf-available/zz-ocsinventory-restapi.conf
 
 sudo sed -i "s|$ENV{OCS_DB_HOST} = 'localhost';;|$ENV{OCS_DB_HOST} = 'localhost';|" $PTH_ZZ_OCS_RESTAPI
-sudo sed -i "s|$ENV{OCS_DB_PORT} = '3306';|$ENV{OCS_DB_PORT} = '$OCS_PORT';|" $PTH_ZZ_OCS_RESTAPI
-sudo sed -i "s|$ENV{OCS_DB_NAME} = 'ocsweb';|$ENV{OCS_DB_LOCAL} = '$OCS_DB';|" $PTH_ZZ_OCS_RESTAPI
-sudo sed -i "s|$ENV{OCS_DB_LOCAL} = 'ocsweb';|$ENV{OCS_DB_LOCAL} = '$OCS_DB';|" $PTH_ZZ_OCS_RESTAPI
-sudo sed -i "s|$ENV{OCS_DB_USER} = 'ocs';|$ENV{OCS_DB_USER} = '$OCS_USER';|" $PTH_ZZ_OCS_RESTAPI
-sudo sed -i "s|$ENV{OCS_DB_PWD} = 'ocs';|$ENV{OCS_DB_PWD} = '$OCS_PASSWORD';|" $PTH_ZZ_OCS_RESTAPI
+sudo sed -i "s|$ENV{OCS_DB_PORT} = '3306';|$ENV{OCS_DB_PORT} = '$OCS_DB_PORT';|" $PTH_ZZ_OCS_RESTAPI
+sudo sed -i "s|$ENV{OCS_DB_NAME} = 'ocsweb';|$ENV{OCS_DB_LOCAL} = '$OCS_DB_NAME';|" $PTH_ZZ_OCS_RESTAPI
+sudo sed -i "s|$ENV{OCS_DB_LOCAL} = 'ocsweb';|$ENV{OCS_DB_LOCAL} = '$OCS_DB_NAME';|" $PTH_ZZ_OCS_RESTAPI
+sudo sed -i "s|$ENV{OCS_DB_USER} = 'ocs';|$ENV{OCS_DB_USER} = '$OCS_DB_USER';|" $PTH_ZZ_OCS_RESTAPI
+sudo sed -i "s|$ENV{OCS_DB_PWD} = 'ocs';|$ENV{OCS_DB_PWD} = '$OCS_DB_PASSWORD';|" $PTH_ZZ_OCS_RESTAPI
 sudo sed -i "s|$ENV{OCS_DB_SSL_ENABLED} = 0;|$ENV{OCS_DB_SSL_ENABLED} = 0;|" $PTH_ZZ_OCS_RESTAPI
 
-export PTH_Z_OCS=/etc/apache2/conf-available/z-ocsinventory-server.conf
+PTH_Z_OCS=/etc/apache2/conf-available/z-ocsinventory-server.conf
 
 sudo sed -i "s|PerlSetEnv OCS_DB_HOST localhost|PerlSetEnv OCS_DB_HOST localhost|" $PTH_Z_OCS
-sudo sed -i "s|PerlSetEnv OCS_DB_PORT 3306|PerlSetEnv OCS_DB_PORT $OCS_PORT|" $PTH_Z_OCS
-sudo sed -i "s|PerlSetEnv OCS_DB_NAME ocsweb|PerlSetEnv OCS_DB_NAME $OCS_DB|" $PTH_Z_OCS
-sudo sed -i "s|PerlSetEnv OCS_DB_LOCAL ocsweb|PerlSetEnv OCS_DB_LOCAL $OCS_DB|" $PTH_Z_OCS
-sudo sed -i "s|PerlSetEnv OCS_DB_USER ocs|PerlSetEnv OCS_DB_USER $OCS_USER|" $PTH_Z_OCS
-sudo sed -i "s|PerlSetVar OCS_DB_PWD ocs|PerlSetVar OCS_DB_PWD $OCS_PASSWORD|" $PTH_Z_OCS
+sudo sed -i "s|PerlSetEnv OCS_DB_PORT 3306|PerlSetEnv OCS_DB_PORT $OCS_DB_PORT|" $PTH_Z_OCS
+sudo sed -i "s|PerlSetEnv OCS_DB_NAME ocsweb|PerlSetEnv OCS_DB_NAME $OCS_DB_NAME|" $PTH_Z_OCS
+sudo sed -i "s|PerlSetEnv OCS_DB_LOCAL ocsweb|PerlSetEnv OCS_DB_LOCAL $OCS_DB_NAME|" $PTH_Z_OCS
+sudo sed -i "s|PerlSetEnv OCS_DB_USER ocs|PerlSetEnv OCS_DB_USER $OCS_DB_USER|" $PTH_Z_OCS
+sudo sed -i "s|PerlSetVar OCS_DB_PWD ocs|PerlSetVar OCS_DB_PWD $OCS_DB_PASSWORD|" $PTH_Z_OCS
 sudo sed -i "s|PerlSetEnv OCS_OPT_ACCEPT_TAG_UPDATE_FROM_CLIENT 0|PerlSetEnv OCS_OPT_ACCEPT_TAG_UPDATE_FROM_CLIENT 1|" $PTH_Z_OCS
 
 ln -s /etc/apache2/conf-available/ocsinventory-reports.conf /etc/apache2/conf-enabled/ocsinventory-reports.conf
@@ -126,7 +130,10 @@ ln -s /etc/apache2/conf-available/zz-ocsinventory-restapi.conf /etc/apache2/conf
 
 sudo a2enconf ocsinventory-reports
 sudo a2enconf z-ocsinventory-server
+sudo a2enconf zz-ocsinventory-restapi
+
 chown -R www-data:www-data /var/lib/ocsinventory-reports
+
 systemctl restart apache2
 
 echo
@@ -136,11 +143,11 @@ echo "Acesse via broswer http://$HOST_IP/ocsreports"
 echo
 echo "Informe os dados configurados na instalação:"
 echo
-echo "Usuário Mysql: $OCS_USER"
-echo "Senha Mysql: $OCS_PASSWORD"
-echo "Nome da base de dados: $OCS_DB"
-echo "Servidor Mysql: localhost"
+echo "Nome da base de dados: $OCS_DB_NAME"
 echo "Porta MySQL : 3306"
+echo "Servidor Mysql: localhost"
+echo "Usuário Mysql: $OCS_DB_USER"
+echo "Senha Mysql: $OCS_DB_PASSWORD"
 echo "Habilitar SSL: Não"
 echo
 echo
