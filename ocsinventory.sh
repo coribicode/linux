5a76aacd14045136cf7c5947f4d6661769d83a18
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-15 21:21:55 -0300

    ocsinventory.sh

---
+++ ocsinventory.sh
@@ -0,0 +1,65 @@
+apt install -y build-essential sudo wget git curl perl make cmake gcc unzip gnupg systemd-timesyncd ca-certificates
+
+apt install -y net-tools pciutils smartmontools read-edid nmap
+
+apt install -y php-zip php-pclzip php-gd php-curl php-json php-mbstring php-xml php-mysql
+
+apt_dist=ocsinventory
+
+keyring_gpg_path=/usr/share/keyrings/$apt_dist.gpg
+
+dist_codenome=$(cat /etc/*release* | grep CODENAME | cut -d "=" -f 2)
+
+uri_gpg=http://deb.ocsinventory-ng.org/pubkey.gpg
+uri_deb="http://deb.ocsinventory-ng.org/debian/"
+
+curl -fsSL $uri_gpg | sudo gpg --dearmor -o $keyring_gpg_path
+
+cat >> /etc/apt/sources.list.d/$apt_dist.list << EOL
+deb [signed-by=$keyring_gpg_path] $uri_deb $dist_codenome main
+EOL
+
+apt update
+
+apt install -y libapache2-mod-perl2 \
+libapache-dbi-perl \
+libapache-db-perl \
+libapache2-mod-php \
+libxml-simple-perl \
+libxml-perl \
+libdbd-mysql-perl \
+libnet-ip-perl \
+libsoap-lite-perl \
+libsoap-wsdl-perl \
+libio-compress-perl \
+libcrypt-ssleay-perl \
+libnet-snmp-perl \
+libproc-pid-file-perl \
+libproc-daemon-perl \
+libnet-netmask-perl \
+libarchive-zip-perl \
+libmojolicious-perl \
+libswitch-perl \
+libyaml-perl \
+libghc-libyaml-dev \
+apache2-dev \
+libplack-handler-anyevent-fcgi-perl
+
+yes | cpan -i YAML
+cpan -i XML::Entities
+cpan -i Apache2::SOAP
+cpan -i SOAP::Transport::HTTP
+
+sudo apt install -y ocsinventory
+
+chown -Rv www-data.www-data /var/lib/ocsinventory-reports
+
+sed -i 's/OCS_OPT_GENERATE_OCS_FILES_SNMP 0/OCS_OPT_GENERATE_OCS_FILES_SNMP 0 \
+  # PerlSetEnv SNMP_LINK_TAG 0\
+  PerlSetEnv OCS_OPT_SNMP_LINK_TAG 0/g' /etc/apache2/conf-enabled/z-ocsinventory-server.conf
+
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+systemctl reload apache2.service
+systemctl restart apache2
+
