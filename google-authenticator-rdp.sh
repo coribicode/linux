b9b36a50475d0c4ccf308765e3997572ce7a189f
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-24 16:34:01 -0300

    google-authenticator-rdp.sh

--- google-authenticator-rdp.sh
+++ google-authenticator-rdp.sh
@@ -11,4 +11,7 @@ EOF
 
 sudo systemctl restart xrdp
 
-google-authenticator -t -D -W -C -f -r 3 -R 30
+echo "Execute o comando abaixo na sessão do usuário"
+echo "google-authenticator -t -D -W -C -f -r 3 -R 30"
+echo
+echo "Sincronize o QRCode com seu OTP de preferência"
