2ed701f75f2a1f3ba530b803dbcbf92176c9e9b7
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-27 09:58:40 -0300

    update-upgrade.sh

--- update-upgrade.sh
+++ update-upgrade.sh
@@ -1,19 +1,28 @@
-DEBIAN_VERSION_CODENAME=$(cat /etc/*release* | grep VERSION_CODENAME | cut -d '=' -f 2)
+VERSION_CODENAME=$(cat /etc/*release* | grep ^VERSION_CODENAME | cut -d '=' -f 2)
+ID=$(cat /etc/*release* | grep ^ID | cut -d '=' -f2)
+
+TYPES='deb deb-src'
+URIS='http://deb.debian.org/debian'
+URIS_SEC='http://deb.debian.org/debian-security'
+SUITES="$VERSION_CODENAME $VERSION_CODENAME-updates $VERSION_CODENAME-backports"
+COMPONENTES='main contrib non-free non-free-firmware'
+SIGNED='/usr/share/keyrings/debian-archive-keyring.gpg'
+PATH_SOURCE="/etc/apt/sources.list.d/$ID.sources"
 
 mv /etc/apt/sources.list /etc/apt/sources.list.bkp
 
-cat <<EOF>> /etc/apt/sources.list.d/debian.sources
-Types: deb deb-src
-URIs: http://deb.debian.org/debian 
-Suites: $DEBIAN_VERSION_CODENAME $DEBIAN_VERSION_CODENAME-updates $DEBIAN_VERSION_CODENAME-backports
-Components: main contrib non-free non-free-firmware
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+cat <<EOF>> $PATH_SOURCE 
+Types: $TYPES
+URIs: $URIS
+Suites: $SUITES
+Components: $COMPONENTES
+Signed-By: $SIGNED
 
-Types: deb deb-src
-URIs: http://deb.debian.org/debian-security
-Suites: $DEBIAN_VERSION_CODENAME-security
-Components: main contrib non-free non-free-firmware
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+Types: $TYPES
+URIs: $URIS_SEC
+Suites: $VERSION_CODENAME-security
+Components: $COMPONENTES
+Signed-By: $SIGNED
 EOF
 
 apt update && apt upgrade -y && systemctl daemon-reload
