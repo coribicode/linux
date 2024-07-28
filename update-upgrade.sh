185c45eff39de937428f8cebc9add747938e7632
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-28 17:58:15 -0300

    repository-stable.sh

--- update-upgrade.sh
+++ update-upgrade.sh
@@ -5,7 +5,8 @@ ID=$(cat /etc/*release* | grep ^ID | cut -d '=' -f2)
 TYPES='deb deb-src'
 URIS='http://deb.debian.org/debian'
 URIS_SEC='http://deb.debian.org/debian-security'
-SUITES="$VERSION_CODENAME $VERSION_CODENAME-updates $VERSION_CODENAME-backports"
+SUITES="stable stable-updates stable-backports"
+SUITES_SEC="stable-security"
 COMPONENTES='main contrib non-free non-free-firmware'
 SIGNED='/usr/share/keyrings/debian-archive-keyring.gpg'
 PATH_SOURCE="/etc/apt/sources.list.d/$ID.sources"
@@ -13,6 +14,7 @@ PATH_SOURCE="/etc/apt/sources.list.d/$ID.sources"
 mv /etc/apt/sources.list /etc/apt/sources.list.bkp
 
 cat <<EOF>> $PATH_SOURCE 
+## STABLE
 Types: $TYPES
 URIs: $URIS
 Suites: $SUITES
@@ -21,7 +23,7 @@ Signed-By: $SIGNED
 
 Types: $TYPES
 URIs: $URIS_SEC
-Suites: $VERSION_CODENAME-security
+Suites: $SUITES_SEC
 Components: $COMPONENTES
 Signed-By: $SIGNED
 EOF
