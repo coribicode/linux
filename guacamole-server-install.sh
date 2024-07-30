3aba72f16a38adc5c8229e41fdcd9a5e3858d714
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-29 22:52:10 -0300

    guacamole-server-install.sh

--- guacamole-server-install.sh
+++ guacamole-server-install.sh
@@ -42,7 +42,7 @@ else
   apt update 2>&1 | grep "E:"
 fi
 
-if grep PACKAGE_NAME $INSTALLER
+if grep PACKAGE_NAME $INSTALLER 2>&1
   then
     sed -i "s|PACKAGE_NAME|$package_list|g" $INSTALLER
     sh $INSTALLER
