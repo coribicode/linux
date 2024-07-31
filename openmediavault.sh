1df58787f69e9cb7ea7e22dc884d20ee6bee6b69
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-31 00:57:40 -0300

    openmediavault.sh

--- openmediavault.sh
+++ openmediavault.sh
@@ -5,18 +5,30 @@ key_uri='https://packages.openmediavault.org/public/archive.key'
 ## Prioridade IPV4 ##
 echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
 
-apt-get install -y wget gnupg sudo systemd-timesyncd ca-certificates
+echo "[OpenMediaVault]: Instalando dependências..."
+apt-get install -y wget gnupg sudo systemd-timesyncd ca-certificates 2>&1
+echo "[OpenMediaVault]: Verificando repositórios ..."
 
-cat > /etc/apt/sources.list.d/$apt_dist.list << EOF
+FILE=/etc/apt/sources.list.d/$apt_dist.list
+if [ -e $FILE ]
+  then
+    echo "[OpenMediaVault]: Repositórios OK!"
+  else
+    echo "[OpenMediaVault]: Atualizando repositórios ..."
+cat > $FILE << EOF
 deb [signed-by=$keyring_gpg_path] http://packages.openmediavault.org/public/ sandworm main
 deb [signed-by=$keyring_gpg_path] https://openmediavault.github.io/packages/ sandworm main
 EOF
-wget --quiet --output-document=- $key_uri | gpg --dearmor --yes --output "$keyring_gpg_path"
+wget --quiet --output-document=- $key_uri | gpg --dearmor --yes --output "$keyring_gpg_path" 2>&1
+    echo "[OpenMediaVault]: Repositórios - OK!"
+fi
 
-apt update
-apt upgrade -y
+systemctl daemon-reload
+apt update 2>&1
+apt upgrade -y 2>&1
 systemctl daemon-reload
 
+echo "[OpenMediaVault]: Instalando OpenMediaVault ..."
 export LANG=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=none
@@ -25,30 +37,30 @@ apt-get --yes --auto-remove --show-upgraded \
     --no-install-recommends \
     --option DPkg::Options::="--force-confdef" \
     --option DPkg::Options::="--force-confold" \
-    install openmediavault openvswitch-switch
+    install openmediavault openvswitch-switch 2>&1
 
-FILE=/etc/netplan/*openmediavault*.yaml
+sleep 2
+echo "[OpenMediaVault]: Reconfigurando Conexão de Rede ..."
 NIC=$(ip -br -4 a | grep UP | cut -d ' ' -f 1)
 
-cp $FILE /etc/netplan/openmediavault.yaml.bkp
-
-cat >> $FILE << "EOF"
+cat >> /etc/netplan/*openmediavault*.yaml << "EOF"
 ethernets:
   $NIC:
     dhcp4: true
 EOF
 
 sudo netplan apply
-
-echo
-echo "Buscando as últimas atualizações do OpenMediaVault..."
-echo
-sudo apt --only-upgrade install openmediavault
-sudo omv-upgrade
-echo
+echo "[OpenMediaVault]: Conexão de Rede - OK!"
 echo
 echo "Acesse http://$(hostname -I | cut -d ' ' -f 1)"
 echo
 echo "Login: admin"
 echo "Senha: openmediavault"
 echo
+
+#echo
+#echo "Buscando as últimas atualizações do OpenMediaVault..."
+#echo
+#sudo apt --only-upgrade install openmediavault
+#sudo omv-upgrade
+#echo
