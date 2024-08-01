f363f7e0994c6b897e77fb96b1b347bd920d9c8c
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-08-01 09:22:24 -0300

    debian-repository.sh

--- debian_stable_repository.sh
+++ debian_stable_repository.sh
@@ -6,46 +6,51 @@ URIS_SEC='http://deb.debian.org/debian-security'
 SUITES="$CODENAME $CODENAME-updates $CODENAME-backports"
 SUITES_SEC="$CODENAME-security"
 COMPONENTES='main contrib non-free non-free-firmware'
-SIGNED='/usr/share/keyrings/debian-archive-keyring.gpg'
+SIGNED="/usr/share/keyrings/$CODENAME-archive-keyring.gpg"
 PATH_SOURCE="/etc/apt/sources.list.d/$CODENAME.sources"
 
+echo "[ Sistema ]: Verificando ... "
+echo
+sleep 2
 
 if [ -e $PATH_SOURCE ];
   then
     echo
-    echo "Repositório [$PATH_SOURCE]: OK!"
+    ls /etc/apt/sources.list.d/ | grep "$CODENAME.sources"
+    echo "[ Repositório ]: OK!"
     sleep 2
   else
     echo
-    echo "Repositório []: Atualizando..."
+    echo "[ Repositório ]: Configurando ..."
     sleep 2
     mv /etc/apt/sources.list /etc/apt/sources.list.bkp
     sleep 2
-    touch $PATH_SOURCE
-    echo "## DEBIAN STABLE RESPOSITORY" >> $PATH_SOURCE
-    echo "Types: $TYPES" >> $PATH_SOURCE
-    echo "URIs: $URIS" >> $PATH_SOURCE
-    echo "Suites: $SUITES" >> $PATH_SOURCE
-    echo "Components: $COMPONENTES" >> $PATH_SOURCE
-    echo "Signed-By: $SIGNED" >> $PATH_SOURCE
-    echo "" >> $PATH_SOURCE
-    echo "Types: $TYPES" >> $PATH_SOURCE
-    echo "URIs: $URIS_SEC" >> $PATH_SOURCE
-    echo "Suites: $SUITES_SEC" >> $PATH_SOURCE
-    echo "Components: $COMPONENTES" >> $PATH_SOURCE
-    echo "Signed-By: $SIGNED" >> $PATH_SOURCE
-    sleep 2
-    echo "Repositório [$PATH_SOURCE]: OK!"
+cat > $PATH_SOURCE << "EOF"
+## $CODENAME RESPOSITORY ##
+Types: $TYPES
+URIs: $URIS
+Suites: $SUITES
+Components: $COMPONENTES
+Signed-By: $SIGNED
+
+Types: $TYPES
+URIs: $URIS_SEC
+Suites: $SUITES_SEC
+Components: $COMPONENTES
+Signed-By: $SIGNED
+EOF
+    ls /etc/apt/sources.list.d/ | grep "$CODENAME.sources"
+    echo "[ Repositório ]: OK!"
     sleep 2
 fi
 
 echo
-echo "SISTEMA: Atualizando..."
+echo "[ Sistema ]: Atualizando ..."
 apt update -qq 2>&1 | grep "E:"
 apt upgrade -qqy 2>&1 | grep "E:"
 systemctl daemon-reload 2>&1 | grep "E:"
 apt --fix-broken -qq install 2>&1 | grep "E:"
 sleep 2
-echo "SISTEMA: OK!"
+echo "[ Sistema ]: OK!"
 echo
 sleep 2
