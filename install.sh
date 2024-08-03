0b1b7d401f1ec985f4dd28e172635331232351fa
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-08-03 15:07:59 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -15,39 +15,37 @@ package_list="PACKAGE_NAME"
 
 for package in $package_list
   do
-    package_installed=$(dpkg --get-selections | grep ^"$package" | grep -w install)
-    sleep 2
-    if [ -n "$package_installed" ] ;
-      then
-      echo "Pacote [ $package ]: OK!"
-      echo "--------------------------------------------------------------------"
+  package_installed=$(dpkg --get-selections | grep ^"$package" | grep -w install)
+  if [ -n "$package_installed" ] ;
+    then
+    echo "Pacote [ $package ]: OK!"
+    echo "--------------------------------------------------------------------"
     else
-      check_repo=$(apt-cache search $package| grep ^"$package ")
-      if [ ! -n "$check_repo" ]
+    check_repo=$(apt-cache search $package| grep ^"$package ")
+    if [ ! -n "$check_repo" ]
+      then
+      echo "Pacote [ $package ]: ERROR - Não foi possível instalar porque não foi encontrado nos repositórios"
+      echo "--------------------------------------------------------------------"
+      exit ## >>>>>>> SAI DA INSTALAÇÃO SE HOUVER ERRO <<<<<<<<<<< ##
+      else
+      echo "Pacote [ $package ]: Não instalado!"
+      sleep 2
+      echo "Pacote [ $package ]: Instalando pacote..."
+      sleep 2
+      apt install -qq -y $package 2>&1 | grep "E:"
+      check_package_installed=$(dpkg --get-selections | grep ^"$package" | grep -w install)
+      sleep 2
+      if [ -n "$check_package_installed" ] ;
         then
+        echo "Pacote [ $package ]: Instalado!"
+        echo "--------------------------------------------------------------------"
+        else
         echo "Pacote [ $package ]: Não Instalado!"
         sleep 2
-        echo "Pacote [ $package ]: ERROR - Não foi possível instalar porque não foi encontrado nos repositórios"
+        echo "Houve erro na instalação, verifique os logs e tente novamnete"
         echo "--------------------------------------------------------------------"
         exit ## >>>>>>> SAI DA INSTALAÇÃO SE HOUVER ERRO <<<<<<<<<<< ##
       fi
-    echo "Pacote [ $package ]: Não instalado!"
-    sleep 2
-    echo "Pacote [ $package ]: Instalando pacote..."
-    sleep 2
-    apt install -qq -y $package 2>&1 | grep "E:"
-    check_package_installed=$(dpkg --get-selections | grep ^"$package" | grep -w install)
-    sleep 2
-    if [ -n "$check_package_installed" ] ;
-      then
-      echo "Pacote [ $package ]: Instalado!"
-      echo "--------------------------------------------------------------------"
-    else
-      echo "Pacote [ $package ]: Não Instalado!"
-      sleep 2
-      echo "Houve erro na instalação, verifique os logs e tente novamnete"
-      echo "--------------------------------------------------------------------"
-      exit ## >>>>>>> SAI DA INSTALAÇÃO SE HOUVER ERRO <<<<<<<<<<< ##
     fi
   fi
 done
