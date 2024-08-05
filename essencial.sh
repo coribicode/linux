290abc6e9934b5479ee10f10f8c677eac9c5997c
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-08-05 11:18:05 -0300

    essencial.sh

--- essencial.sh
+++ essencial.sh
@@ -1,1 +1,24 @@
-apt install -y build-essential lsb-release apt-transport-https module-assistant ca-certificates software-properties-common aptitude sudo wget git gnupg curl cmake g++ gcc make automake autoconf net-tools plocate lzip tar unzip
+apt-get install -y \
+build-essential \
+lsb-release \
+apt-transport-https \
+module-assistant \
+ca-certificates \
+software-properties-common \
+aptitude \
+sudo \
+wget \
+git \
+gnupg \
+curl \
+cmake \
+g++ \
+gcc \
+make \
+automake \
+autoconf \
+net-tools \
+plocate \
+lzip \
+tar \
+unzip
