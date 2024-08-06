a3e013452dfafd2aa5a152101eb57bd6c11515dc
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-08-06 15:12:05 -0300

    debian_repository.sh

--- debian_repository.sh
+++ debian_repository.sh
@@ -9,16 +9,16 @@ COMPONENTES='main contrib non-free non-free-firmware'
 SIGNED="/usr/share/keyrings/debian-archive-keyring.gpg"
 PATH_SOURCE="/etc/apt/sources.list.d/$CODENAME.sources"
 
-if [ ! -e $PATH_SOURCE ]
-  echo "-------------------------------------------------"
-  echo "[ Repositório - $CODENAME ]: Verificando ..."
+echo "-------------------------------------------------"
+echo "[ Repositório - $CODENAME ]: Verificando ..."
 sleep 2
-  then
-    echo "[ Repositório - $CODENAME ]: Configurando ..."
+if [ -e $PATH_SOURCE ]
+then
+echo "[ Repositório - $CODENAME ]: - OK!"
+else
+echo "[ Repositório - $CODENAME ]: Configurando ..."
 sleep 2
-    
-mv /etc/apt/sources.list /etc/apt/sources.list.bkp 2>&1
-
+mv /etc/apt/sources.list /etc/apt/sources.list.bkp 2>&1 | grep "mv"
 cat > $PATH_SOURCE << EOF
 ## $CODENAME RESPOSITORY ##
 Types: $TYPES
@@ -26,66 +26,55 @@ URIs: $URIS
 Suites: $SUITES
 Components: $COMPONENTES
 Signed-By: $SIGNED
-
 Types: $TYPES
 URIs: $URIS_SEC
 Suites: $SUITES_SEC
 Components: $COMPONENTES
 Signed-By: $SIGNED
 EOF
-
-    echo "[ Repositório - $CODENAME ]: OK!"
-    echo "-------------------------------------------------"
-    else
-        echo "[ Repositório - $CODENAME ]: - OK!"
-    echo "-------------------------------------------------"
+echo "[ Repositório - $CODENAME ]: OK!"
 fi
-sleep 2
 
+echo "-------------------------------------------------"
+echo "[ NOVOS Repositórios ]: Verificando ..."
+sleep 2
 if [ ! -e ~/repo ]
-  echo "[ NOVOS Repositórios ]: Verificando ..."
-sleep 2
-  then
-    echo "[ NOVOS Repositórios ]: Não há"
-    echo "-------------------------------------------------"
-  else
-  sh ~/repo
-    echo "[ NOVOS Repositórios ]: OK!"
-    echo "-------------------------------------------------"
+then
+echo "[ NOVOS Repositórios ]: Não há"
+else
+sh ~/repo
+echo "[ NOVOS Repositórios ]: OK!"
 fi
-sleep 2
 
-if grep ^'precedence ::ffff:0:0/96  100'  /etc/gai.conf > /dev/null
-  echo "[ Prioridade IPv4 ]: Verificando ..."
+echo "-------------------------------------------------"
+echo "[ Prioridade IPv4 ]: Verificando ..."
 sleep 2
-  then
-  echo "[ Prioridade IPv4 ]: OK!"
-  echo "-------------------------------------------------"
-  else
-  echo "[ Prioridade IPv4 ]: Configurando ... "
+if grep ^'precedence ::ffff:0:0/96  100'  /etc/gai.conf > /dev/null
+then
+echo "[ Prioridade IPv4 ]: OK!"
+else
+echo "[ Prioridade IPv4 ]: Configurando ... "
 echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
 sleep 2
-  echo "[ Prioridade IPv4 ]: OK!"
-  echo "-------------------------------------------------"
+echo "[ Prioridade IPv4 ]: OK!"
 fi
-sleep 2
 
-if grep ^'export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin' ~/.bashrc > /dev/null
-  echo "[ Fix LDCONFIG ]: Verificando ..."
+echo "-------------------------------------------------"
+echo "[ Fix LDCONFIG ]: Verificando ..."
 sleep 2
-  then
-  echo "[ Fix LDCONFIG ]: OK!"
-  echo "-------------------------------------------------"
-  else
-  echo "[ Fix LDCONFIG ]: Configurando ... "
+if grep ^'export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin' ~/.bashrc > /dev/null
+then
+echo "[ Fix LDCONFIG ]: OK!"
+else
+echo "[ Fix LDCONFIG ]: Configurando ... "
 echo 'export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin' >> ~/.bashrc
 source /etc/profile
-  echo "[ Fix LDCONFIG ]: OK!"
-  echo "-------------------------------------------------"
+echo "[ Fix LDCONFIG ]: OK!"
 fi
-sleep 2
 
+echo "-------------------------------------------------"
 echo "[ Sistema ]: Atualizando ..."
+sleep 2
 apt-get update -qq 2>&1 | grep "E:"
 apt-get upgrade -qqy 2>&1 | grep "E:"
 systemctl daemon-reload 2>&1 | grep "E:"
@@ -93,4 +82,3 @@ apt-get --fix-broken -qq install | grep "E:"
 sleep 2
 echo "[ Sistema ]: OK!"
 echo "-------------------------------------------------"
-sleep 2
