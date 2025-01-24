0d6a14dfc80afd607edceb84a9a9ddb5b5056932
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-01-24 16:51:30 -0300

    kvm_bridge_net.sh

--- kvm_bridge_net.sh
+++ kvm_bridge_net.sh
@@ -9,19 +9,30 @@ BRIDGE_NAME=br0
 PHYSICAL_INTERFACE=$(ip -4 a | grep $(hostname -I | cut -d ' ' -f 1) | grep -o '[^ ]*$')   # Nome da interface de rede física, ajuste conforme sua configuração
 
 echo "Instalando pacotes necessários .."
-apt install -y bridge-utils ifupdown > /dev/null
+apt-get install -y bridge-utils ifupdown > /dev/null
 echo
+if [ -e /etc/network/interfaces.d/$BRIDGE_NAME ] > /dev/null
+then
+echo "[ $PHYSICAL_INTERFACE já configurado para $BRIDGE_NAME! ]"
+else
 echo "Configuração do bridge $BRIDGE_NAME no arquivo /etc/network/interfaces"
+sleep 5
 # Criar backup do arquivo de configuração de rede
-cp /etc/network/interfaces /etc/network/interfaces.$PHYSICAL_INTERFACE.bkp
-cat >> /etc/network/interfaces << EOF
+# cp /etc/network/interfaces /etc/network/interfaces.$PHYSICAL_INTERFACE.bkp
 
-# Configuração da interface física para o bridge $BRIDGE_NAME
+sed -i 's/^/#/' /etc/network/interfaces.d/$PHYSICAL_INTERFACE
+cat >> /etc/network/interfaces.d/$PHYSICAL_INTERFACE << EOF
 auto $PHYSICAL_INTERFACE
 iface $PHYSICAL_INTERFACE inet manual
-up ip link set dev $PHYSICAL_INTERFACE up
-down ip link set dev $PHYSICAL_INTERFACE down
+EOF
+
+# Configuração da interface física para o bridge $BRIDGE_NAME
+#auto $PHYSICAL_INTERFACE
+#iface $PHYSICAL_INTERFACE inet manual
+#up ip link set dev $PHYSICAL_INTERFACE up
+#down ip link set dev $PHYSICAL_INTERFACE down
 # -----------------------------------------------------
+cat >> /etc/network/interfaces.d/$BRIDGE_NAME << EOF
 auto $BRIDGE_NAME
 iface $BRIDGE_NAME inet dhcp
 bridge_ports $PHYSICAL_INTERFACE
@@ -29,12 +40,11 @@ bridge_fd 0
 bridge_maxwait 0
 bridge_stp on
 EOF
-cat /etc/network/interfaces 
+fi
 echo "-------------------------------------------------"
 echo "Reiniciar a rede para aplicar as configurações."
 systemctl restart networking
 systemctl restart NetworkManager
-systemctl restart network-online.target
 echo "-------------------------------------------------"
 echo "Verificar se o bridge $BRIDGE_NAME foi criado com sucesso"
 brctl show
