fa64c4371d6fa05e13b1e1b1079ff165eb07d07b
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-01-24 10:26:04 -0300

    mt5.sh

--- mt5.sh
+++ mt5.sh
@@ -37,6 +37,29 @@ sudo -u $USER wine $PWD/.cache/wine/mt5setup.exe /auto
 
 apt install yad at-spi2-core chromium lxtask -y
 
+# -- Configurações para passagem de som via X11 Forwarding ------------------------------------
+
+apt install -y alsa-utils pulseaudio firmware-linux
+sudo -u $USER pulseaudio --start
+sed -i 's|#load-module module-native-protocol-tcp|load-module module-native-protocol-tcp auth-anonymous=1|g' 
+sudo usermod -aG audio $USER
+
+cat << EOF > /etc/pulse/daemon.conf
+realtime-scheduling = yes
+default-fragments = 4
+default-fragment-size-msec = 25
+EOF
+
+sed -i 's|#AllowAgentForwarding yes|AllowAgentForwarding yes|g' /etc/ssh/sshd_config
+sed -i 's|#AllowTcpForwarding yes|AllowTcpForwarding yes|g' /etc/ssh/sshd_config
+sed -i 's|#X11Forwarding yes|X11Forwarding yes|g' /etc/ssh/sshd_config
+sed -i 's|#X11UseLocalhost yes|X11UseLocalhost yes|g' /etc/ssh/sshd_config
+
+export LIBGL_ALWAYS_SOFTWARE=1
+export MOZ_WEBGL_DISABLE_UNSAFE=1
+
+" ---------------------------------------------------------------------------------------------
+
 cat << EOF > /opt/painel
 #!/bin/bash
 # Criação do painel com três botões
