33607dc4562af4e39956c7eeae8d8151c75d2c1b
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-01-24 21:02:22 -0300

    x11forwarding.sh

--- x11forwarding.sh
+++ x11forwarding.sh
@@ -3,7 +3,8 @@
 apt install -y alsa-utils pulseaudio pulseaudio-module-zeroconf firmware-linux dbus-x11 x11-apps x11-xfs-utils s3dx11gate
 apt install -y libx11-freedesktop-desktopentry-perl librust-x11+xinput-dev  librust-x11rb+xinput-dev libx11-6 libx11-dev clang
 sudo -u $USER pulseaudio --start
-sed -i 's|#load-module module-native-protocol-tcp|load-module module-native-protocol-tcp auth-anonymous=1|g' /etc/pulse/default.pa
+sed -i 's|#load-module module-native-protocol-tcp|load-module module-native-protocol-tcp|g' /etc/pulse/default.pa
+sed -i 's|#load-module module-zeroconf-publish|load-module module-zeroconf-publish|g' /etc/pulse/default.pa
 sudo usermod -aG audio $USER
 
 cat << EOF >> /etc/pulse/daemon.conf
