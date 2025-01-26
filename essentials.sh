d49b494751aad55b295c9a3bbf3392f286d26cba
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-01-26 12:37:35 -0300

    essentials.sh

--- essentials.sh
+++ essentials.sh
@@ -1,1 +1,15 @@
-apt-get install -y build-essential firmware-linux lsb-release apt-transport-https module-assistant ca-certificates software-properties-common aptitude sudo wget git curl tar unzip lzip xorg xvfb xauth pulseaudio alsa-utils alsa-tools libasound2 libasound2-dev udns-utils net-tools rfkill neofetch screenfetch cmake g++ gcc make automake autoconf flex bison bc gdb gnupg gnupg1 gnupg2 gnutls-bin libjwt-gnutls-dev 
+PACKAGES="build-essential firmware-linux lsb-release apt-transport-https module-assistant ca-certificates software-properties-common aptitude sudo wget git curl tar unzip lzip xorg xvfb xauth pulseaudio alsa-utils alsa-tools libasound2 libasound2-dev udns-utils net-tools rfkill neofetch screenfetch cmake g++ gcc make automake autoconf flex bison bc gdb gnupg gnupg1 gnupg2 gnutls-bin libjwt-gnutls-dev"
+echo
+curl -LO https://raw.githubusercontent.com/davigalucio/linux/main/install.sh 2>/dev/null | grep "E:"
+INSTALLER="install.sh"
+
+echo "[ Essentials ]: Inicio"
+if grep PACKAGE_NAME $INSTALLER > /dev/null
+  then
+    sed -i "s|PACKAGE_NAME|$PACKAGES|g" $INSTALLER
+    sh $INSTALLER
+  else
+    sh $INSTALLER
+fi
+echo "[ Essentials ]: Fim."
+sleep 2
