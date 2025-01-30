63623cd3644fbc44769b1300b0b8031e528ce441
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-01-30 14:14:48 -0300

    mt5.sh

--- mt5.sh
+++ mt5.sh
@@ -76,10 +76,10 @@ mkdir $winedir/.cache/wine
 chown -R $USER:$USER $winedir
 
 wget -P $winedir/.cache/wine https://dl.winehq.org/wine/wine-mono/9.4.0/wine-mono-9.4.0-x86.msi
-sudo -u $USER wine msiexec /i $winedir/.cache/wine/wine-mono-9.4.0-x86.msi
+sudo -u $USER WINEPREFIX="$winedir/.wine" wine msiexec /i $winedir/.cache/wine/wine-mono-9.4.0-x86.msi
 
 wget -P $winedir/.cache/wine https://dl.winehq.org/wine/wine-gecko/2.47.4/wine-gecko-2.47.4-x86_64.msi
-sudo -u $USER wine msiexec -i $winedir/.cache/wine/wine-gecko-2.47.4-x86_64.msi
+sudo -u $USER WINEPREFIX="$winedir/.wine" wine msiexec -i $winedir/.cache/wine/wine-gecko-2.47.4-x86_64.msi
 
 sudo -u $USER WINEPREFIX="$winedir/.wine" WINEARCH=win64 wine wineboot -u -f -r
 sudo -u $USER WINEPREFIX="$winedir/.wine" wineserver -k
