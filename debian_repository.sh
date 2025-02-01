3ee5125468fd58634c19e1c1b67c9876f2e93102
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-02-01 15:54:57 -0300

    debian_repository.sh

--- debian_repository.sh
+++ debian_repository.sh
@@ -12,12 +12,12 @@ PATH_SOURCE="/etc/apt/sources.list.d/$CODENAME.sources"
 echo "-------------------------------------------------"
 echo "[ Prioridade IPv4 ]: Verificando ..."
 sleep 2
-if grep ^'precedence ::ffff:0:0/96  100'  /etc/gai.conf > /dev/null
+if grep ^'precedence ::ffff:0:0/96  100' /etc/gai.conf > /dev/null
 then
 echo "[ Prioridade IPv4 ]: OK!"
 else
 echo "[ Prioridade IPv4 ]: Configurando ... "
-echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
+sed -i 's|"#precedence ::ffff:0:0/96  100|precedence ::ffff:0:0/96  100|g'" >> /etc/gai.conf
 sleep 2
 echo "[ Prioridade IPv4 ]: OK!"
 fi
