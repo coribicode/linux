be6e76de5452837c0bea9a33d9a284c5371a1376
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-02-07 14:35:22 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Criado por: Davi dos Santos Galúcio - 2024
 # Verifica e instala pacote automaticamente
 
-package_list="PACKAGE_NAME"
+package_list="PACKAGE_NAME" 2> /dev/null
 echo "--------------------------------------------------------------------"
 for package in $package_list
   do
@@ -24,7 +24,7 @@ for package in $package_list
       echo "Pacote [ $package ]: Instalando pacote..."
       sleep 2
       export DEBIAN_FRONTEND=noninteractive
-      apt-get install -qq -y $package > /dev/null
+      apt-get install -qq -y $package 2> /dev/null
       check_package_installed=$(dpkg --get-selections | grep ^"$package" | grep -w install)
       sleep 2
       if [ -n "$check_package_installed" ] ;
