b16dfef2943831da6ca5afae739f29adab53f4e8
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-04-10 19:05:20 -0300

    xpra.sh

--- xpra.sh
+++ xpra.sh
@@ -9,7 +9,7 @@ apt install xpra xpra-client-gtk3 xpra-codecs-extras xpra-codecs xpra-common pyt
 apt install xpra-filesystem xpra-common xpra-client xpra-client-gtk3 xpra-server xpra-x11 xpra-audio xpra-codecs xpra-codecs-extras xpra-codecs-nvidia
 
 # xpra start :100 --start-child=xclock --bind-tcp=0.0.0.0:10000 --html=on --start=xterm
-xpra start :100 --bind-tcp=0.0.0.0:10000 --html=on --start=xterm
+xpra start :100 --bind-tcp=0.0.0.0:10000 --html=on --start=xterm --env=XPRA_FORCE_COLOR_DEPTH=24   --env=DISPLAY=:100   --dpi=96
 
 xpra list
 
