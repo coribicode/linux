3d2e5db4f3406b42cfd77b8a00cd626a70d2e207
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-04-11 16:24:12 -0300

    xpra.sh

--- xpra.sh
+++ xpra.sh
@@ -12,8 +12,27 @@ apt install dbus-x11 ibus gir1.2-rsvg-2.0 libsystemd-dev libvpx7 libwebp7 -y
 apt install python3 python3-rencode python3-paramiko python3-dnspython python3-zeroconf python3-netifaces python3-cups python3 python3-gi-cairo python3-setproctitle python3-xdg python3-pyinotify -y
 
 # xpra start :100 --start-child=xclock --bind-tcp=0.0.0.0:10000 --html=on --start=xterm
-xpra start :100 --bind-tcp=0.0.0.0:10000 --html=on --start=xterm --env=XPRA_FORCE_COLOR_DEPTH=24   --env=DISPLAY=:100   --dpi=96
+# xpra start :100 --bind-tcp=0.0.0.0:10000 --html=on --start=xterm --env=XPRA_FORCE_COLOR_DEPTH=32   --env=DISPLAY=:100   --dpi=144
 
+xpra start :100 \
+  --bind-tcp=0.0.0.0:10000 \
+  --start-child="wine /caminho/para/jogo.exe" \
+  --encodings=nvenc,h264,vpx,vp9 \
+  --quality=100 \
+  --min-quality=70 \
+  --speed=50 \
+  --opengl=yes \
+  --dpi=144 \
+  --video-scaling=off \
+  --no-pulseaudio \
+  --env=XPRA_ALLOW_ROOT=1 \
+  --no-mmap \
+  --no-pings \
+  --html=on \
+  --env=XPRA_FORCE_COLOR_DEPTH=32 \
+  --env=DISPLAY=:100 \
+  --no-notifications
+  
 xpra list
 
 # xpra stop :100
