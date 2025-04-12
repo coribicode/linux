f1e734ec7d5b3e7d6c35a113d87af23791087e36
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-04-12 16:42:23 -0300

    xpra.sh

--- xpra.sh
+++ xpra.sh
@@ -11,46 +11,4 @@ apt install -y dbus-x11 ibus uglifyjs quilt xserver-xorg-dev xutils-dev xserver-
 apt install -y gstreamer1.0-pulseaudio gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly
 apt install -y libpam-dev libjs-jquery libjs-jquery-ui libnvidia-encode1 libx264-dev libvpx-dev libturbojpeg-dev libwebp-dev libgtk-3-dev libsystemd-dev libvpx7 libwebp7 libx11-dev libxtst-dev libxcomposite-dev libxdamage-dev libxres-dev libxkbfile-dev liblz4-dev
 apt install -y python3-dev python3-opengl python3-numpy python3-cairo-dev python3-pil python-gi-dev python3-dbus python3-cryptography python3-netifaces python3-yaml python3-rencode python3-paramiko python3-dnspython python3-zeroconf python3-netifaces python3-cups python3-gi-cairo python3-setproctitle python3-xdg python3-pyinotify
-apt install -y lxterminal
 
-XPRA_APP=lxterminal
-XPRA_USER=user001
-XPRA_USER_PASSWORD=123
-sudo useradd -m -s /bin/bash $XPRA_USER && echo "$XPRA_USER:$XPRA_USER_PASSWORD" | sudo chpasswd
-usermod -aG $(groups $USER | cut -d ":" -f2 | sed -e 's/^[[:space:]]*//g' | tr ' ' ',') $XPRA_USER
-
-XPRA_USER_UID=$(id -u $XPRA_USER)
-XPRA_USER_PORT=$(id -u $XPRA_USER)
-XPRA_USER_DISPLAY=$(id -u $XPRA_USER)
-XPRA_USER_HOME=$(getent passwd $XPRA_USER | cut -d: -f6)
-
-mkdir /run/user/$XPRA_USER_UID
-chown -R $XPRA_USER:$XPRA_USER /run/user/$XPRA_USER_UID
-chmod -R 0700 /run/user/$XPRA_USER_UID
-XDG_RUNTIME_DIR=/run/user/$XPRA_USER_UID
-echo "export XDG_RUNTIME_DIR=\"$XDG_RUNTIME_DIR\"" >> $XPRA_USER_HOME/.bashrc
-
-cd / && sudo -u $XPRA_USER xpra start :$XPRA_USER_DISPLAY \
- --bind-tcp=0.0.0.0:$XPRA_USER_PORT \
- --start=$XPRA_APP \
- --encodings=h264,vp9 \
- --quality=100 \
- --min-quality=80 \
- --speed=100 \
- --opengl=yes \
- --dpi=144 \
- --video-scaling=off \
- --no-pulseaudio \
- --env=XPRA_ALLOW_ROOT=1 \
- --no-mmap \
- --html=on \
- --env=XPRA_FORCE_COLOR_DEPTH=32 \
- --env=DISPLAY=:$XPRA_USER_DISPLAY \
- --no-notifications \
- --socket-dir=$XDG_RUNTIME_DIR
-  
-sudo -u $XPRA_USER xpra list
-
-# sudo -u $USER xpra stop :100
-# xpra start :100 --start-child=xclock --bind-tcp=0.0.0.0:10000 --html=on --start=xterm
-# xpra start :100 --bind-tcp=0.0.0.0:10000 --html=on --start=xterm --env=XPRA_FORCE_COLOR_DEPTH=32   --env=DISPLAY=:100   --dpi=144
