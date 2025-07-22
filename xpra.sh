0762834fdcb3676093dbcbc42d68eb233d874c03
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-07-22 13:15:35 -0300

    xpra.sh

--- xpra.sh
+++ xpra.sh
@@ -31,16 +31,16 @@ PACKAGES_DEPENDECES="python3-distutils python3-pip python3-dev python3-opengl py
 INSTALLING
 sudo -u $USER pip3 install --user --break-system-packages PyOpenGL_accelerate
 
+NAME_PACKAGE="XPRA DRIVERS"
+PACKAGES_DEPENDECES="i965-va-driver x264 va-driver-all vdpau-driver-all intel-media-va-driver-non-free pulseaudio"
+INSTALLING
+
 NAME_PACKAGE="OTHERS PACKAGES"
 PACKAGES_DEPENDECES="gstreamer1.0-pulseaudio gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly vainfo dbus-x11 ibus ibus-gtk3 ibus-pinyin uglifyjs quilt xserver-xorg-dev xutils-dev xserver-xorg-video-dummy xvfb keyboard-configuration brotli gir1.2-rsvg-2.0 yasm cython3 devscripts build-essential lintian debhelper pandoc gnome-backgrounds openssh-client sshpass"
 INSTALLING
 
 NAME_PACKAGE="XPRA LIBS"
 PACKAGES_DEPENDECES="libva-drm2 libva-x11-2 libva-drm2 libva-x11-2 libpam-dev libjs-jquery libjs-jquery-ui libnvidia-encode1 libx264-dev libvpx-dev libturbojpeg-dev libwebp-dev libgtk-3-dev libsystemd-dev libvpx7 libwebp7 libx11-dev libxtst-dev libxcomposite-dev libxdamage-dev libxres-dev libxkbfile-dev liblz4-dev"
-INSTALLING
-
-NAME_PACKAGE="XPRA DRIVERS"
-PACKAGES_DEPENDECES="i965-va-driver x264 va-driver-all vdpau-driver-all intel-media-va-driver-non-free"
 INSTALLING
 
 FILE_GPG=/etc/apt/trusted.gpg.d/xpra.gpg
