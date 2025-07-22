0423cdbf27c95d7f5858823a8a418edf1c4d65e9
davigalucio <82713318+davigalucio@users.noreply.github.com>
2025-07-21 23:56:45 -0300

    x11forwarding.sh

--- x11forwarding.sh
+++ x11forwarding.sh
@@ -3,47 +3,103 @@
 apt install -y alsa-utils pulseaudio pulseaudio-module-zeroconf firmware-linux dbus-x11 x11-apps x11-xfs-utils s3dx11gate
 apt install -y libx11-freedesktop-desktopentry-perl librust-x11+xinput-dev  librust-x11rb+xinput-dev libx11-6 libx11-dev clang
 sudo -u $USER pulseaudio --start
-sed -i 's|#load-module module-native-protocol-tcp|load-module module-native-protocol-tcp|g' /etc/pulse/default.pa
-sed -i 's|#load-module module-zeroconf-publish|load-module module-zeroconf-publish|g' /etc/pulse/default.pa
 sudo usermod -aG audio $USER
 
-PATH_SOURCE=/etc/pulse/daemon.conf
-if [ -e $PATH_SOURCE ]
+FILE_PULSE=/etc/pulse/default.pa
+if [ -e $FILE_PULSE ]
 then
-ADD_CONFIG="realtime-scheduling = yes"
-echo $ADD_CONFIG >> $PATH_SOURCE
-ADD_CONFIG="default-fragments = 4"
-echo $ADD_CONFIG >> $PATH_SOURCE
-ADD_CONFIG="default-fragment-size-msec = 25"
-echo $ADD_CONFIG >> $PATH_SOURCE
+cp $FILE_PULSE $FILE_PULSE.bkp
+ADD_CONFIG="#load-module module-native-protocol-tcp"
+if grep "$ADD_CONFIG" $FILE_PULSE
+then
+sed -i "s|$ADD_CONFIG|$(echo $ADD_CONFIG | sed 's/#//g')|g" $FILE_PULSE
+fi
+ADD_CONFIG="#load-module module-zeroconf-publish"
+if grep "$ADD_CONFIG" $FILE_PULSE
+then
+sed -i "s|$ADD_CONFIG|$(echo $ADD_CONFIG | sed 's/#//g')|g" $FILE_PULSE
+fi
 else
-echo "[ $PATH_SOURCE ]: Não encontrado!"
+echo "[ $FILE_PULSE ]: Não encontrado!"
+fi
+
+FILE_PULSE=/etc/pulse/daemon.conf
+if [ -e $FILE_PULSE ]
+then
+cp $FILE_PULSE $FILE_PULSE.bkp
+ADD_CONFIG="; realtime-scheduling = yes"
+if grep "$ADD_CONFIG" $FILE_PULSE
+then
+sed -i "s|$ADD_CONFIG|$(echo $ADD_CONFIG | sed 's/; //g')|g" $FILE_PULSE
+fi
+ADD_CONFIG="; default-fragments = 4"
+if grep "$ADD_CONFIG" $FILE_PULSE
+then
+sed -i "s|$ADD_CONFIG|$(echo $ADD_CONFIG | sed 's/; //g')|g" $FILE_PULSE
+fi
+ADD_CONFIG="; default-fragment-size-msec = 25"
+if grep "$ADD_CONFIG" $FILE_PULSE
+then
+sed -i "s|$ADD_CONFIG|$(echo $ADD_CONFIG | sed 's/; //g')|g" $FILE_PULSE
+fi
+else
+echo "[ $FILE_PULSE ]: Não encontrado!"
 fi
 
 sudo -u $USER pulseaudio -k
 sudo -u $USER pulseaudio --start
 
-sed -i 's|#AllowAgentForwarding yes|AllowAgentForwarding yes|g' /etc/ssh/sshd_config
-sed -i 's|#AllowTcpForwarding yes|AllowTcpForwarding yes|g' /etc/ssh/sshd_config
-sed -i 's|#X11Forwarding yes|X11Forwarding yes|g' /etc/ssh/sshd_config
-sed -i 's|#X11UseLocalhost yes|X11UseLocalhost yes|g' /etc/ssh/sshd_config
+FILE_SSHD_CONFIG=/etc/ssh/sshd_config
+if [ -e $FILE_SSHD_CONFIG ]
+then
+cp $FILE_SSHD_CONFIG $FILE_SSHD_CONFIG.bkp
+ADD_CONFIG_SSHD="#AllowAgentForwarding yes"
+if grep "$ADD_CONFIG_SSHD" $FILE_SSHD_CONFIG
+then
+sed -i "s|$ADD_CONFIG_SSHD|$(echo $ADD_CONFIG_SSHD | sed 's/#//g')|g" $FILE_SSHD_CONFIG
+fi
+ADD_CONFIG_SSHD="#AllowTcpForwarding yes"
+if grep "$ADD_CONFIG_SSHD" $FILE_SSHD_CONFIG
+then
+sed -i "s|$ADD_CONFIG_SSHD|$(echo $ADD_CONFIG_SSHD | sed 's/#//g')|g" $FILE_SSHD_CONFIG
+fi
+ADD_CONFIG_SSHD="#X11Forwarding yes"
+if grep "$ADD_CONFIG_SSHD" $FILE_SSHD_CONFIG
+then
+sed -i "s|$ADD_CONFIG_SSHD|$(echo $ADD_CONFIG_SSHD | sed 's/#//g')|g" $FILE_SSHD_CONFIG
+fi
+ADD_CONFIG_SSHD="#X11UseLocalhost yes"
+if grep "$ADD_CONFIG_SSHD" $FILE_SSHD_CONFIG
+then
+sed -i "s|$ADD_CONFIG_SSHD|$(echo $ADD_CONFIG_SSHD | sed 's/#//g')|g" $FILE_SSHD_CONFIG
+fi
+else
+echo "[ $FILE_VAR ]: Não encontrado!"
+fi
 
-PATH_VAR="/etc/environment"
+FILE_VAR="/etc/environment"
+if [ -e $FILE_VAR ]
+then
+cp $FILE_VAR $FILE_VAR.bkp
 
 ADD_VAR="LIBGL_ALWAYS_SOFTWARE=1"
-if grep "$ADD_VAR" $PATH_VAR  > /dev/null
+if grep "$ADD_VAR" $FILE_VAR  > /dev/null
 then
 echo "VAR $ADD_VAR: Variavel já cadastrado!"
 else
-echo "$ADD_VAR" >> $PATH_VAR 
+echo "$ADD_VAR" >> $FILE_VAR 
 fi
 
 ADD_VAR="MOZ_WEBGL_DISABLE_UNSAFE=1"
-if grep "$ADD_VAR" $PATH_VAR > /dev/null
+if grep "$ADD_VAR" $FILE_VAR > /dev/null
 then
 echo "VAR $ADD_VAR: Variavel já cadastrado!"
 else
-echo "$ADD_VAR" >> $PATH_VAR 
+echo "$ADD_VAR" >> $FILE_VAR 
+fi
+
+else
+echo "[ $FILE_VAR ]: Não encontrado!"
 fi
 
 source /etc/environment
