cb48ab367a3ead6b82aedc0802c6a9e2b8598e8b
coribi <82713318+coribicode@users.noreply.github.com>
2025-08-12 06:36:21 -0300

    essentials13.sh

---
+++ essentials13.sh
@@ -0,0 +1,15 @@
+PACKAGES="build-essential firmware-linux lsb-release apt-transport-https module-assistant ca-certificates aptitude sudo wget git curl perl tar unzip lzip xorg xvfb xauth pulseaudio alsa-utils alsa-tools libasound2 libasound2-dev udns-utils net-tools rfkill screenfetch cmake g++ gcc make automake autoconf flex bison bc gdb gnupg gnupg1 gnupg2 gnutls-bin libjwt-gnutls-dev"
+echo
+curl -LO https://raw.githubusercontent.com/davigalucio/linux/main/install.sh > /dev/null 2>&1
+INSTALLER="install.sh"
+
+echo "[ Essentials ]: Inicio"
+if grep PACKAGE_NAME $INSTALLER > /dev/null 2>&1
+  then
+    sed -i "s|PACKAGE_NAME|$PACKAGES|g" $INSTALLER
+    sh $INSTALLER
+  else
+    sh $INSTALLER
+fi
+echo "[ Essentials ]: Fim."
+sleep 2
